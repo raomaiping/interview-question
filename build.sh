00599cffe1db91ccbbf18e3fd7a3160501448398
raomaiping <2582395486@qq.com>
2022-10-12 22:12:54 +0800

    提交一些vue3面试题

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ cd ./docs/.vuepress/dist
 # 创建本地 Git 仓库
 git init 
 # 创建并切换分支
-git checkout -b main
+git checkout -b gh-pages
 # 添加和提交
 git add -A
 git commit -m '部署'
 # 指定 origin 和分支，直接强推
-git push -f git@github.com:raomaiping/interview-question.git main
+git push -f git@github.com:raomaiping/interview-question.git gh-pages
